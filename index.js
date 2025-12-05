145f50e183f09e0b13c6b4b266384827fd79e733
makoesibanda <makoesibanda@gmail.com>
2025-12-05 01:43:06 +0000

    After Lab 6: CALLING APIs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const ejs = require('ejs');
 const mysql = require('mysql2');
 const path = require('path');
+const request = require('request');   // used to call external APIs (OpenWeather)
+
 
 const app = express();
 const port = 8000;
@@ -61,6 +63,52 @@ app.use('/users', usersRoutes);
 
 const booksRoutes = require('./routes/books');
 app.use('/books', booksRoutes);
+
+ // Weather route using EJS (weather.ejs)
+ app.get('/weather', function (req, res) {
+
+    let apiKey = 'ef604080244c70a937f9627093383f15';
+    // If the user has not entered a city yet, just show the page
+    if (req.query.city === undefined) {
+        res.render('weather', { weatherMsg: null });
+    } 
+    else {
+        let city = req.query.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+        request(url, function (err, response, body) {
+            if (err) {
+                res.render('weather', { weatherMsg: 'Error fetching weather data' });
+            } 
+           else {
+
+    var weather = JSON.parse(body);
+
+            // Error handling 
+            if (weather !== undefined && weather.main !== undefined) {
+
+                var wmsg = 
+                    'It is ' + weather.main.temp + ' degrees in ' + weather.name + '<br>' +
+                    'Humidity: ' + weather.main.humidity + '<br>' +
+                    'Wind speed: ' + weather.wind.speed + '<br>' +
+                    'Weather: ' + weather.weather[0].description;
+
+                res.render('weather', { weatherMsg: wmsg });
+
+            }
+            else {
+                res.render('weather', { weatherMsg: 'No data found' });
+            }
+        }
+
+
+        });
+
+    }
+
+});
+
+
 
 // Start server
 app.listen(port, () => {
